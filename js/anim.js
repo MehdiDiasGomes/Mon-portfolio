473b930d8ec028865024cfd1c4817b62b90203d1
Dias_Gomes <diasgomes.mehdi270@gmail.com>
2023-12-28 19:52:04 +0100

    first commit

---
+++ js/anim.js
@@ -0,0 +1,29 @@
+const sr = ScrollReveal ({
+    distance: '50px',
+    duration: 1500,
+    reset: false
+});
+
+//=====HOME=====//
+
+sr.reveal('.home-text', {origin: 'left', delay: 100})
+sr.reveal('.home .home-img', {origin: 'right', delay: 100})
+
+//=====COMPETENCES=====//
+
+sr.reveal('.contact', {origin: 'top', delay: 100})
+sr.reveal('.competences .title', {origin: 'top', delay: 100})
+sr.reveal('.container-comp .container .comp1', {origin: 'left', delay: 100})
+sr.reveal('.container-comp .container .comp2', {origin: 'left', delay: 600})
+sr.reveal('.container-comp .container .comp3', {origin: 'left', delay: 1100})
+sr.reveal('.container-comp .container .comp4', {origin: 'left', delay: 1600})
+sr.reveal('.container-comp .container .comp5', {origin: 'left', delay: 2100})
+sr.reveal('.container-comp .container .comp6', {origin: 'left', delay: 2600})
+
+//=====PARCOURS=====//
+
+sr.reveal('.title', {origin: 'top', delay: 300})
+sr.reveal('.box1', {origin: 'right', delay: 100})
+sr.reveal('.box2', {origin: 'left', delay: 100})
+sr.reveal('.box3', {origin: 'right', delay: 100})
+sr.reveal('.box4', {origin: 'left', delay: 100})
---
+++ js/main.js
@@ -0,0 +1,44 @@
+//=====MENU=====//
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuHamburger = document.querySelector(".menu");
+  const navLinks = document.querySelector(".navbar");
+
+  menuHamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("mobilemenu");
+  });
+
+  const links = document.querySelectorAll('.navbar .nav-links a');
+
+  links.forEach(link => {
+      link.addEventListener('click', () => {
+          navLinks.classList.remove('mobilemenu');
+      });
+  });
+});
+
+//=====TYPE-WRITER=====//
+
+var app = document.querySelector('.home-text h3');
+
+var typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 35,
+  });
+  
+  typewriter
+    .pauseFor(1000)
+    .typeString('Apprenti développeur ')
+    .pauseFor(0)
+    .typeString('<span style="color: #FFFFFF;">JS</span> !')
+    .pauseFor(300)
+    .deleteChars(4)
+    .typeString('<span style="color: #FFFFFF;">HTML</span> !')
+    .pauseFor(300)
+    .deleteChars(6)
+    .typeString('<span style="color: #FFFFFF;">CSS</span> !')
+    .pauseFor(300)
+    .deleteChars(5)
+    .typeString('<span style="color: #FFFFFF;">REACT</span> !')
+    .pauseFor(300)
+    .start();
