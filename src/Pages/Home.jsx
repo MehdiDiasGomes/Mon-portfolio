7544888851b5fd5030a003c34f3f000ace2badc3
Dias_Gomes <diasgomes.mehdi270@gmail.com>
2024-02-10 20:11:08 +0100

    ✨ Ajout CV

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Mehdi from '../Assets/img/Mehdi.jpg'
+import monCV from "../Assets/doc/CV_Mehdi_DIAS_GOMES.pdf"
 
 const Home = () => {
     return (
@@ -13,8 +14,8 @@ const Home = () => {
                     <p className='pcScreen:px-64 md:text-xl sm:text-paragMobile sm:px-10 text-parag text-secondary text-center'>Je suis un apprenti développeur web full-stack de <span className='font-bold text-primary'>20 ans</span>, à la recherche d&apos;un <span className='font-bold text-primary'>contrat en alternance</span> pour préparer un <span className='font-bold text-primary'>Bac+3</span> à Metz Numeric School pour l&apos;année <span className='font-bold text-primary'>2024-2025</span>. Mon objectif est d&apos;acquérir des compétences dans le développement web et de contribuer de manière proactive à des projets passionnants au sein d&apos;une équipe dynamique.</p>
                 </div>
                 <div className='flex gap-7 text-primary'>
-                    <a className='bg-primary md:text-xl text-black p-5 flex justify-center items-center font-bold sm:text-paragMobile' href="#">Prenez contact</a>
-                    <a className='text-primary md:text-xl border-2 border-primary p-5 flex items-center justify-center hover:text-black hover:bg-primary duration-300 font-bold sm:text-paragMobile' href="#">Ouvrir mon CV</a>
+                    <a className='bg-primary md:text-xl text-black p-5 flex justify-center items-center font-bold sm:text-paragMobile' href="mailto:diasgomes.mehdicours@gmail.com">Prenez contact</a>
+                    <a className='text-primary md:text-xl border-2 border-primary p-5 flex items-center justify-center hover:text-black hover:bg-primary duration-300 font-bold sm:text-paragMobile' href={monCV} target='blank'>Ouvrir mon CV</a>
                 </div>
             </div>
         </div>
