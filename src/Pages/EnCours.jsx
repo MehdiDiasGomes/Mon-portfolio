7892debe54c3e21f86a92786079914cbcd4fc05e
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-02-18 18:37:13 +0100

    📱 Responsive "en cours"

--- src/Pages/EnCours.jsx
+++ src/Pages/EnCours.jsx
@@ -5,8 +5,8 @@ const EnCours = () => {
     return (
         <div className='w-full h-[100vh] flex justify-center items-center'>
             <div className='flex flex-col gap-3 items-center justify-center'>
-                <h2 className='text-primary text-4xl'>Page en cours de développement 🕤</h2>
-                <Link to="/" className='text-secondary hover:text-primary hover:scale-125	 duration-300 text-2xl'><i className="ri-arrow-left-line"></i>Retour en arrière</Link>
+                <h2 className='text-primary md:text-4xl sm:text-1xl'>Page en cours de développement 🕤</h2>
+                <Link to="/" className='text-secondary hover:text-primary hover:scale-125 duration-300 md:text-2xl sm:text-1xl'><i className="ri-arrow-left-line"></i>Retour en arrière</Link>
             </div>
         </div>
     )
