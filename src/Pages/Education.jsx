0dece488251ae5745e115f70d135f72e2b4b3c7e
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-02-18 19:18:05 +0100

    💚 Amélioration de reveal

--- src/Pages/Education.jsx
+++ src/Pages/Education.jsx
@@ -5,21 +5,22 @@ import Fade from 'react-reveal/Fade';
 
 const Experiences = () => {
     return (
+        <div className='flex flex-col gap-20' id='educ'>
+            <div className='flex justify-center'>
+                <h2 className='md:text-5xl sm:text-3xl bg-gradient-to-r from-gradientPrimary to-gradientSecondary text-transparent bg-clip-text font-bold'>ÉDUCATION</h2>
+            </div>
 
-        <Fade left>
-            <div className='flex flex-col gap-20' id='educ'>
-                <div className='flex justify-center'>
-                    <h2 className='md:text-5xl sm:text-3xl bg-gradient-to-r from-gradientPrimary to-gradientSecondary text-transparent bg-clip-text font-bold'>ÉDUCATION</h2>
-                </div>
 
-                <div className="flex flex-col gap-20 pcScreen:px-64 sm:px-10 md:px-14 lg:px-0">
+            <div className="flex flex-col gap-20 pcScreen:px-64 sm:px-10 md:px-14 lg:px-0">
+                <Fade left>
                     {EducationConst.map((e, index) => (
                         <EducComp key={index} img={e.img} titre={e.titre} date={e.date} parag={e.parag} />
                     ))}
-                </div>
+                </Fade>
             </div>
-        </Fade>
 
+
+        </div>
     )
 }
 
