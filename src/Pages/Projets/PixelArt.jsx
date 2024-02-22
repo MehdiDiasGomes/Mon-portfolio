7b358195d33d022ea7ace212f196ab63a2b31f54
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-02-22 18:05:21 +0100

    ✨ Prettier

--- src/Pages/Projets/PixelArt.jsx
+++ src/Pages/Projets/PixelArt.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { ProjetsContenuConst } from '../../Constants'
-import ProjetsContenuComp from '../../Components/ProjetsContenu/index'
+import React from "react";
+import ProjetsContenuComp from "../../Components/ProjetsContenu/index";
+import { ProjetsContenuConst } from "../../Constants";
 
 const PixelArt = () => {
-    return (
-        <div>
-            {ProjetsContenuConst.map((e, index) => (
-                <ProjetsContenuComp
-                    key={index}
-                    titre={e.titre}
-                    img={e.img}
-                    desc={e.desc}
-                    maquettingTitre={e.maquettingTitre}
-                    devTitre={e.devTitre}
-                    hebergementTitre={e.hebergementTitre}
-                    logoMaq={e.logoMaq} l
-                    logoDev={e.logoDev}
-                    logoHeb={e.logoHeb} />
-            ))}
-        </div>
-    )
-}
+  return (
+    <div>
+      {ProjetsContenuConst.map((e, index) => (
+        <ProjetsContenuComp
+          key={index}
+          titre={e.titre}
+          img={e.img}
+          desc={e.desc}
+          maquettingTitre={e.maquettingTitre}
+          devTitre={e.devTitre}
+          hebergementTitre={e.hebergementTitre}
+          logoMaq={e.logoMaq}
+          l
+          logoDev={e.logoDev}
+          logoHeb={e.logoHeb}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default PixelArt;
