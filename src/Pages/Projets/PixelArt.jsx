0e6e3ebe7ba85d228baa18a3f40a6d3915e35980
MehdiDiasGomes <diasgomes.mehdi270@gmail.com>
2024-04-21 14:10:29 +0200

    💄 Ajout animation pages projets

--- src/Pages/Projets/PixelArt.jsx
+++ src/Pages/Projets/PixelArt.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import ProjetsTemplate from '../../Components/ProjetsContenu/index'
+import Fade from 'react-reveal/Fade'
 
 const PixelArt = ({ data }) => {
   return (
-    <ProjetsTemplate
-      key={data.titre}
-      titre={data.titre}
-      img={data.img}
-      desc={data.desc}
-      maquettingTitre={data.maquettingTitre}
-      devTitre={data.devTitre}
-      hebergementTitre={data.hebergementTitre}
-      logoMaq={data.logoMaq}
-      logoDev={data.logoDev}
-      logoHeb={data.logoHeb}
-      lienOuvrir={data.lienOuvrir}
-      lienCode={data.lienCode}
-    />
+    <Fade>
+      <ProjetsTemplate
+        key={data.titre}
+        titre={data.titre}
+        img={data.img}
+        desc={data.desc}
+        maquettingTitre={data.maquettingTitre}
+        devTitre={data.devTitre}
+        hebergementTitre={data.hebergementTitre}
+        logoMaq={data.logoMaq}
+        logoDev={data.logoDev}
+        logoHeb={data.logoHeb}
+        lienOuvrir={data.lienOuvrir}
+        lienCode={data.lienCode}
+      />
+    </Fade>
   )
 }
 
--- src/Pages/Projets/ToDoList.jsx
+++ src/Pages/Projets/ToDoList.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import ProjetTemplate from '../../Components/ProjetsContenu/index'
+import Fade from 'react-reveal/Fade'
 
 const ToDoList = ({ data }) => {
   return (
-    <ProjetTemplate
-      key={data.titre}
-      titre={data.titre}
-      img={data.img}
-      desc={data.desc}
-      maquettingTitre={data.maquettingTitre}
-      devTitre={data.devTitre}
-      hebergementTitre={data.hebergementTitre}
-      logoMaq={data.logoMaq}
-      logoDev={data.logoDev}
-      logoHeb={data.logoHeb}
-      lienOuvrir={data.lienOuvrir}
-      lienCode={data.lienCode}
-    />
+    <Fade>
+      <ProjetTemplate
+        key={data.titre}
+        titre={data.titre}
+        img={data.img}
+        desc={data.desc}
+        maquettingTitre={data.maquettingTitre}
+        devTitre={data.devTitre}
+        hebergementTitre={data.hebergementTitre}
+        logoMaq={data.logoMaq}
+        logoDev={data.logoDev}
+        logoHeb={data.logoHeb}
+        lienOuvrir={data.lienOuvrir}
+        lienCode={data.lienCode}
+      />
+    </Fade>
   )
 }
 
